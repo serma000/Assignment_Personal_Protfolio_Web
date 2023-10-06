517d0ffc56143282ab3bb7a3bb2cb333bb269ccc
serma000 <123521719+serma000@users.noreply.github.com>
2023-10-06 14:36:00 -0400

    homepage
    needs to debug homepage error, because it is not rendering

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
   Date: 2023-10-01 */
 
 const express = require("express");
-const path = require("path")
+const path = require("path");
 const app = express();
 
 // Serve static files from the 'node_modules' directory
@@ -14,9 +14,17 @@ app.use('/node_modules', express.static(__dirname + '/node_modules'));
 //serve static file from the 'public' directory
 app.use('/public',express.static(path.join(__dirname + '/public')));
 
+/*set the view engine for Express application to "ejs. */
 app.set("view engine","ejs");
 
-app.get("/homeEJS",(_req,res)=>{
+// Define a route to serve the PDF file
+app.get('/pdf', (req, res) => {
+    const pdfFilePath = path.join(__dirname, 'public', 'resume.pdf');
+    res.sendFile(pdfFilePath);
+});
+
+/*defines a route */
+app.get("/",(req, res)=>{
 
     res.render("pages/home");
 });
@@ -24,12 +32,6 @@ app.get("/homeEJS",(_req,res)=>{
 app.get("/aboutEJS", (req, res)=> {
     res.render("pages/about", {   
 });
-});
-
-// Define a route to serve the PDF file
-app.get('/pdf', (req, res) => {
-    const pdfFilePath = path.join(__dirname, 'public', 'resume.pdf');
-    res.sendFile(pdfFilePath);
 });
 
 app.get("/contactEJS", (req, res)=> {
@@ -45,7 +47,7 @@ app.get("/serviceEJS", (req, res)=> {
 });
 });
 
-
+/* this code starts the Express server and makes it listen on port 5000. */
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
 });
